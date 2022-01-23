489ac7c7a721d8988c289c095873b260b86c7abd
Vladimir Chistyakov <b1ackviking@outlook.com>
2022-01-24 03:58:29 +0700

    build(cmake): use versioned toolchains for gcc and clang

---
+++ cmake/clang-13.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER clang-13)
+set(CMAKE_CXX_COMPILER clang++-13)
+set(CMAKE_AR llvm-ar-13)
+set(CMAKE_RANLIB llvm-ranlib-13)
---
+++ cmake/gcc-10.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER gcc-10)
+set(CMAKE_CXX_COMPILER g++-10)
+set(CMAKE_AR gcc-ar-10)
+set(CMAKE_RANLIB gcc-ranlib-10)
