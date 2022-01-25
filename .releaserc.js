13f75dc68e3c01e983f18e1d208859220dabfc77
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-01-25 15:38:08 +0700

    ci: configure semantic-release tools

--- .releaserc.js
+++ .releaserc.js
@@ -2,8 +2,25 @@ module.exports = {
   "branches": "main",
   "tagFormat": "${version}",
   "plugins": [
-    ["@semantic-release/commit-analyzer", {"preset": "conventionalcommits"}],
-    "@semantic-release/release-notes-generator", "@semantic-release/changelog",
+    [
+      "@semantic-release/commit-analyzer", {
+        "preset": "conventionalcommits",
+        "releaseRules": [{"type": "refactor", "release": "patch"}]
+      }
+    ],
+    [
+      "@semantic-release/release-notes-generator", {
+        "preset": "conventionalcommits",
+        "presetConfig": {
+          "types": [
+            {"type": "feat", "section": "Features"},
+            {"type": "fix", "section": "Bug Fixes"},
+            {"type": "refactor", "section": "Refactoring"}
+          ]
+        }
+      }
+    ],
+    "@semantic-release/changelog",
     [
       "@semantic-release/git", {
         "message":
