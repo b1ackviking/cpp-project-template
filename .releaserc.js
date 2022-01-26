bf2027d0ac5550d04b7542b01eadb2fa045e13ef
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-01-26 12:04:26 +0700

    chore: update the changelog generator configuration

--- .releaserc.js
+++ .releaserc.js
@@ -15,7 +15,8 @@ module.exports = {
           "types": [
             {"type": "feat", "section": "Features"},
             {"type": "fix", "section": "Bug Fixes"},
-            {"type": "refactor", "section": "Refactoring"}
+            {"type": "refactor", "section": "Refactoring"},
+            {"type": "perf", "section": "Performance improvements"}
           ]
         }
       }
