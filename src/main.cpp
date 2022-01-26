4c64ea5e844157eea85b5a7d8a412d21e33eaf62
Vladimir Chistyakov <b1ackviking@outlook.com>
2022-01-26 22:08:43 +0700

    ci: cache conan packages

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@
 
 int main(int, char**) {
   using math::fib;
-  // constexpr auto HELLO_MSG = std::to_array("Hello from C++");
-  // std::cout << HELLO_MSG.data() << fib(3) + fib(5) + fib(7) << std::endl;
+  constexpr auto HELLO_MSG = std::to_array("Hello from C++");
+  std::cout << HELLO_MSG.data() << fib(3) + fib(5) + fib(7) << std::endl;
 }
