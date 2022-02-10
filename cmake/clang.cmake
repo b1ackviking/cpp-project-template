bc7f296f896f150a7ef55fbca5a6290a655b1a91
Vladimir Chistyakov <b1ackviking@outlook.com>
2022-02-11 00:58:23 +0700

    build: add clang and gcc toolchains for default versions

---
+++ cmake/clang.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_AR llvm-ar)
+set(CMAKE_RANLIB llvm-ranlib)
---
+++ cmake/gcc.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_AR gcc-ar)
+set(CMAKE_RANLIB gcc-ranlib)
