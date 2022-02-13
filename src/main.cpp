7aa6c75df0af0e3452895c36e860b29ecd8fe44f
Vladimir Chistyakov <b1ackviking@outlook.com>
2022-02-13 23:36:09 +0700

    chore: fix clang-tidy warnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <array>
+#include <functional>
 #include <iostream>
 #include <math/math.hpp>
+#include <numeric>
 
 int main(int, char**) {
-  using math::fib;
+  constexpr std::array INDICES{3, 4, 5, 7};
+  constexpr auto RESULT = std::transform_reduce(
+    INDICES.cbegin(), INDICES.cend(), 0ULL, std::plus{}, math::fib);
   constexpr auto HELLO_MSG = std::to_array("Hello from C++");
-  std::cout << HELLO_MSG.data() << fib(3) + fib(5) + fib(7) << std::endl;
+  std::cout << HELLO_MSG.data() << RESULT << std::endl;
 }
