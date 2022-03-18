725556c4450b954279df1f2af9727fad1f491332
Vladimir Chistyakov <30690670+b1ackviking@users.noreply.github.com>
2022-03-18 21:20:37 +0700

    build(cmake): fix coverage flags

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -232,7 +232,8 @@ function(set_target_warnings target_name)
 endfunction()
 
 function(enable_coverage target_name)
-  if(ENABLE_COVERAGE)
+  if(ENABLE_COVERAGE AND (CMAKE_CXX_COMPILER_ID STREQUAL "GNU"
+                          OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang"))
     target_compile_options(${target_name} INTERFACE --coverage -O0 -g)
     target_link_libraries(${target_name} INTERFACE --coverage)
   endif()
