816d63adc3991420e3db487ffee9ee1f6d155742
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-04-19 17:31:54 +0700

    style: reformat .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,22 +1,24 @@
 module.exports = {
-  "branches": "main",
-  "tagFormat": "${version}",
-  "plugins": [
+  "branches" : "main",
+  "tagFormat" : "${version}",
+  "plugins" :
+  [
     [
       "@semantic-release/commit-analyzer", {
-        "preset": "conventionalcommits",
-        "releaseRules": [{"type": "refactor", "release": "patch"}]
+        "preset" : "conventionalcommits",
+        "releaseRules" : [{"type" : "refactor", "release" : "patch"}]
       }
     ],
     [
       "@semantic-release/release-notes-generator", {
-        "preset": "conventionalcommits",
-        "presetConfig": {
-          "types": [
-            {"type": "feat", "section": "Features"},
-            {"type": "fix", "section": "Bug Fixes"},
-            {"type": "refactor", "section": "Refactoring"},
-            {"type": "perf", "section": "Performance improvements"}
+        "preset" : "conventionalcommits",
+        "presetConfig" : {
+          "types" :
+          [
+            {"type" : "feat", "section" : "Features"},
+            {"type" : "fix", "section" : "Bug Fixes"},
+            {"type" : "refactor", "section" : "Refactoring"},
+            {"type" : "perf", "section" : "Performance improvements"}
           ]
         }
       }
@@ -24,8 +26,7 @@ module.exports = {
     "@semantic-release/changelog",
     [
       "@semantic-release/git", {
-        "message":
-            "chore(release): ${lastRelease.version} → ${nextRelease.version} [skip ci]"
+        "message" : "chore(release): ${lastRelease.version} → ${nextRelease.version} [skip ci]"
       }
     ]
   ]
