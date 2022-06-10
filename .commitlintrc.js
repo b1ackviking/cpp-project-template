8c1f2793477ff5b71444810f0c78a2a0c7508a7f
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-06-10 15:30:20 +0700

    ci: convert commitlint warnings to errors

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends : ["@commitlint/config-conventional"],
   rules : {
-    "header-max-length" : [2, "always", 50],
-    "body-max-line-length" : [2, "always", 72]
+    "header-max-length" : [1, "always", 50],
+    "body-max-line-length" : [1, "always", 72]
   }
 }
