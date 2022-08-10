197369d2ae35bdb5e59a1eee87371800235e2280
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-08-10 17:51:20 +0700

    ci: migrate to ubuntu 22.04 with gcc-12

--- cmake/clang-12.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER clang-12)
-set(CMAKE_CXX_COMPILER clang++-12)
-set(CMAKE_AR llvm-ar-12)
-set(CMAKE_RANLIB llvm-ranlib-12)
--- cmake/clang-13.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER clang-13)
-set(CMAKE_CXX_COMPILER clang++-13)
-set(CMAKE_AR llvm-ar-13)
-set(CMAKE_RANLIB llvm-ranlib-13)
