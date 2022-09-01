18cc0999f6d944d933512277fc22d30791987a0a
Vladimir Chistyakov <v.chistyakov@integrasources.com>
2022-09-01 12:27:30 +0700

    build: update fmt version

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class Example(ConanFile):
             del self.options.fPIC
 
     def requirements(self):
-        self.requires("fmt/9.0.0")
+        self.requires("fmt/9.1.0")
         if not self.conf.get("tools.build:skip_test"):
             self.requires("gtest/1.12.1")
 
