9c0947b5a37e00b2a2fa541a104371254f6ec6ce
Vladimir Chistyakov <b1ackviking@outlook.com>
2023-01-18 17:30:24 +0700

    style: update to clang-format 15

--- lib/math/include/math/math.hpp
+++ lib/math/include/math/math.hpp
@@ -23,8 +23,10 @@ constexpr auto fib(std::uint64_t n) {
   struct vec {
     std::uint64_t v[2]{}; // NOLINT
   };
+
   struct mat {
     std::uint64_t m[2][2]{}; // NOLINT
+
     constexpr mat operator*(const mat& r) const noexcept {
       return mat{{
         {m[0][0] * r.m[0][0] + m[0][1] * r.m[1][0],
@@ -33,10 +35,12 @@ constexpr auto fib(std::uint64_t n) {
          m[1][0] * r.m[0][1] + m[1][1] * r.m[1][1]},
       }};
     }
+
     constexpr mat& operator*=(const mat& r) noexcept {
       *this = *this * r;
       return *this;
     }
+
     constexpr vec operator*(const vec& r) const noexcept {
       return vec{{
         m[0][0] * r.v[0] + m[0][1] * r.v[1],
