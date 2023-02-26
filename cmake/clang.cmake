7ca9d4b58366473d2485a5bc7965306cf32478d2
Vladimir Chistyakov <b1ackviking@outlook.com>
2023-02-27 02:19:19 +0700

    chore: remove unused cmake toolchains

--- cmake/clang.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER clang)
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_AR llvm-ar)
-set(CMAKE_RANLIB llvm-ranlib)
--- cmake/gcc.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-set(CMAKE_AR gcc-ar)
-set(CMAKE_RANLIB gcc-ranlib)
